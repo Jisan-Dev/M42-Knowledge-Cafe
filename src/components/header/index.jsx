74e50f1e8a87126dbec57bbbbc0e9d784214fb84
Jisun696 <istiakkashem35@gmail.com>
2024-03-14 04:56:19 +0600

    added header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,12 @@
+import profile from '../../assets/images/profile.png';
+
 const Header = () => {
-  return <div className="text-red-600 text-8xl font-exo">Header</div>;
+  return (
+    <div className="font-exo container mx-auto p-4 border-b-2 flex justify-between items-center">
+      <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+      <img src={profile} alt="profile image" />
+    </div>
+  );
 };
 
 export default Header;
